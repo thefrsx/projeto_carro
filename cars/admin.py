99d31ec9023dd8b538b882bad7f1a2087fe8c98d
Teo Farias <teofariasfl@gmail.com>
2025-07-09 19:47:48 -0300

    Initial Commit

---
+++ cars/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from cars.models import Car, Brand
+
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
+    search_fields = ('model', 'brand')
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+
+
+# indica o modelo (tabela) e as configurações de admin em admin.py para o Django
+admin.site.register(Car, CarAdmin)
+admin.site.register(Brand, BrandAdmin)
---
+++ cars/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+class Brand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    # a function abaixo diz ao django que ira ser feita uma ligacao na tabela de dados
+    brand = models.ForeignKey(
+        Brand, on_delete=models.PROTECT, related_name='car_brand'
+    )
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    plate = models.CharField(max_length=10, blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    photo = models.ImageField(upload_to='cars/', blank=True, null=True)
+
+    # magic method do Python para fazer retornar um nome mais legível na página do /admin do Django (No projeto)
+    # Nesse caso, vai retornar o campo model como nome do objeto
+    def __str__(self):
+        return self.model
